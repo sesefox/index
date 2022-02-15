268406aafcdb8419524066311a553d3c87457bc4
sesefox <99642537+sesefox@users.noreply.github.com>
2022-02-16 00:47:41 +0800

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
+var title ='色狐狸,发布页,最新发布页,福利发布页，宅男最爱发布页';
 var fabuye={
 	
     'github发布页:': {
@@ -30,8 +31,10 @@ var foot={
 		'txt':'如遇到所有地址无法访问,请耐心等待，更换网址可能需要点时间！'
 		},		
 };
+//图片数量
 var pcimg=8;
 var wapimg=13;
+//下方临时文件
 var temp='请写邮件至<a class="youjian" href="quisade59@gmail.com" target="_blank">quisade59@gmail.com</a>即可获取最新消息';
 
 
